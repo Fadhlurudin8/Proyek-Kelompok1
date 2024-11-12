cfd2dba15db4ac69cc80874f20ca4f7d344d5eab
SuperBypassUdin <24alaudin@mhs.usk.ac.id>
2024-11-13 06:56:18 +0700

    Update main.c

--- Proyek-Kelompok1/main.c
+++ Proyek-Kelompok1/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *input_str(const char string[], int batas_panjang_input)
+char *input_str(const char identitas[], int batas_panjang_input)
 {
     char *input = malloc(batas_panjang_input + 1);
     if (input == NULL) {
@@ -10,18 +10,14 @@ char *input_str(const char string[], int batas_panjang_input)
         exit(EXIT_FAILURE);
     }
 
-    printf("%s", string);
-    if (fgets(input, batas_panjang_input + 1, stdin) == NULL) {
-        free(input);
-        return NULL;
-    }
+    printf("%s: ", identitas); scanf(" %[^\n]", input);
 
     // Hapus karakter newline jika ada
     input[strcspn(input, "\n")] = '\0';
 
     // Memastikan input tidak melebihi batas panjang
     if (strlen(input) > batas_panjang_input) {
-        printf("Tidak boleh lebih dari %d karakter\n", batas_panjang_input);
+        printf("%s tidak boleh lebih dari %d karakter\n", identitas, batas_panjang_input);
         free(input);
         exit(EXIT_FAILURE);
     }
@@ -49,26 +45,20 @@ int main()
     printf("Petunjuk: username (max 20 karakter) dan password (max 10 karakter)\n");
 
     // Ambil input dari pengguna
-    char *username = input_str("Username: ", 20);
-    char *password = input_str("Password: ", 10);
+    char *username = input_str("Username", 20);
+    char *password = input_str("Password", 10);
 
     // Membuat struct akun dan mengisi username dan password
     akun akunku;
-    strncpy(akunku.username, username, sizeof(akunku.username) - 1);
-    akunku.username[sizeof(akunku.username) - 1] = '\0'; // Pastikan null-terminated
-    strncpy(akunku.password, password, sizeof(akunku.password) - 1);
-    akunku.password[sizeof(akunku.password) - 1] = '\0'; // Pastikan null-terminated
+    strcpy(akunku.username, username);
+    strcpy(akunku.password, password);
 
     // Tulis struct akun ke file biner
     fwrite(&akunku, sizeof(akunku), 1, flogin);
 
-    // Bersihkan memori yang dialokasikan
-    free(username);
-    free(password);
-
     fclose(flogin);
 
-    printf("Akun terverifikasi.\n");
+    printf("Akun terverifikasi. lakukan compile pada file main.c dan login dengan format:\n./main username password\n");
 
     return 0;
 }
