d08efc3a5ed711ef81ed00565f4697216ea51b12
Udin51 <148663014+Fadhlurudin8@users.noreply.github.com>
2024-11-13 12:05:44 +0700

    Update main.c

--- Proyek-Kelompok1/main.c
+++ Proyek-Kelompok1/main.c
@@ -4,20 +4,25 @@
 
 char *input_str(const char identitas[], int batas_panjang_input)
 {
+    // Mengalokasikan memori untuk mengembalikan inputan
     char *input = malloc(batas_panjang_input + 1);
     if (input == NULL) {
-        printf("Gagal mengalokasikan memori\n");
+        printf("ERROR! \nGagal mengalokasikan memori\n");
         exit(EXIT_FAILURE);
     }
 
+    // Input dari user
     printf("%s: ", identitas); scanf(" %[^\n]", input);
 
-    // Hapus karakter newline jika ada
-    input[strcspn(input, "\n")] = '\0';
+    // Memastikan tidak ada spasi dalam input
+    if (strchr(input, ' ') != NULL){
+        printf("SIGN UP FAILED! \n%s harus dalam satu kata\n", identitas);
+        exit(EXIT_FAILURE);
+    }
 
     // Memastikan input tidak melebihi batas panjang
     if (strlen(input) > batas_panjang_input) {
-        printf("%s tidak boleh lebih dari %d karakter\n", identitas, batas_panjang_input);
+        printf("SIGN UP FAILED: %s tidak boleh lebih dari %d karakter\n", identitas, batas_panjang_input);
         free(input);
         exit(EXIT_FAILURE);
     }
@@ -25,38 +30,84 @@ char *input_str(const char identitas[], int batas_panjang_input)
     return input;
 }
 
+
+FILE *buka_file(char *nama_file, char *mode_file)
+{
+    FILE *file;
+    if ((file = fopen(nama_file, mode_file)) == NULL){
+            printf("ERROR: Gagal membuka file!\n");
+            exit(EXIT_FAILURE);
+        }
+    return file;
+}
+
+void clear()
+    {printf("\e[1;1H\e[2J");} // perintah untuk membersihkan terminal
+
 typedef struct
 {
     char username[21];
     char password[11];
 } akun;
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("\e[1;1H\e[2J");
-    FILE *flogin;
+    if (argc == 1){
+        clear();
+        printf("\t===== PROYEK PRAKTIKUM PEMROGRAMAN KELOMPOK 1 =====\n");
+        printf("SIGN UP\n");
+        printf("Petunjuk: Username (max 20 karakter), password (max 10 karakter)\n\n");
+        
+        FILE *flogin = buka_file("database/login.bin", "wb");
 
-    if ((flogin = fopen("database/login.bin", "wb")) == NULL){
-        printf("Gagal membuka file!\n");
-        return EXIT_FAILURE;
+        // Ambil input dari pengguna
+        char *username = input_str("Username", 20);
+        char *password = input_str("Password", 10);
+
+        // Membuat struct akun dan mengisi username dan password
+        akun akunku;
+        strcpy(akunku.username, username);
+        strcpy(akunku.password, password);
+
+        // Tulis struct akun ke file biner
+        fwrite(&akunku, sizeof(akunku), 1, flogin);
+
+        // Bersihkan memori yang dialokasikan
+        free(username); free(password);
+
+        fclose(flogin);
+
+        printf("SUCCESS: Akun terverifikasi.\n");
+
+        return EXIT_SUCCESS;
     }
 
-    printf("\nRegistrasi\n");
-    printf("Petunjuk: username (max 20 karakter) dan password (max 10 karakter)\n");
+    else if (argc == 3)
+    {   
+        FILE *flogin = buka_file("database/login.bin", "rb");
 
-    // Ambil input dari pengguna
-    char *username = input_str("Username", 20);
-    char *password = input_str("Password", 10);
+        akun akunku;
+        fread(&akunku, sizeof(akunku), 1, flogin);
 
-    // Membuat struct akun dan mengisi username dan password
-    akun akunku;
-    strcpy(akunku.username, username);
-    strcpy(akunku.password, password);
+        if (strcmp(argv[0], akunku.username) == 1 || strcmp(argv[1], akunku.password) == 1)
+        {   
+            printf("SIGN IN FAILED: Username/password anda salah \nBaca selengkapnya di README.md\n"); 
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            clear();
+            printf("\t===== PROYEK PRAKTIKUM PEMROGRAMAN KELOMPOK 1 =====\n");
+            // inti permainan disini
+        }
+    }
+        
+    else{
+        printf("ERROR!\n Format login: ./main username password \n Baca selengkapnya di README.md"); // 
+    }
 
-    // Tulis struct akun ke file biner
-    fwrite(&akunku, sizeof(akunku), 1, flogin);
-
-    fclose(flogin);
+    return 0;
+}
 
     printf("Akun terverifikasi. lakukan compile pada file main.c dan login dengan format:\n./main username password\n");
 
