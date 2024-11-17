7fce5ca64ad923f6b945fb469532411bd16a36ff
Udin51 <148663014+Fadhlurudin8@users.noreply.github.com>
2024-11-17 15:48:06 +0700

    Update prototype.c

--- prototype.c
+++ prototype.c
@@ -89,6 +89,10 @@ typedef struct
 
 int main(int argc, char *argv[])
 {
+    #ifdef __linux__ || __unix__ || __APPLE__
+        initscr(); cbreak(); noecho();
+    #endif
+    
     // Pembuatan akun
     if (argc == 1){
         clear_terminal();
@@ -205,6 +209,10 @@ int main(int argc, char *argv[])
         clear_terminal();
         printf("\nERROR: \nFormat login: ./<Nama Program> <Username> <Password> \nBaca selengkapnya di README.md\n\n"); // 
     }
+
+    #ifdef __linux__ || __unix__ || __APPLE__
+        endwin();
+    #endif
 
     return 0;
 }
