de57b170d229e78bc147d557388116b6d74ca507
SuperBypassUdin <24alaudin@mhs.usk.ac.id>
2024-11-17 12:25:15 +0700

    Add files via upload

---
+++ database/assets/cross-platform.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// Platform Windows
+#ifdef _WIN32
+    #include <conio.h>
+// Platform UNIX, Linux, dan Mac
+#elif __linux__ || __unix__ || __APPLE__
+    #include <ncurses.h>
+#else
+    #error "Sistem operasi tidak didukung"
+#endif
+
+/* Fungsi untuk input char tanpa buffer 
+   Berguna untuk buat opsi pilihan kepada user*/
+char char_tanpa_buffer()
+{
+    char input;
+    #ifdef _WIN32
+    {
+        input = _getch();
+        return input;
+    }
+    
+    #elif __linux__ || __unix__ || __APPLE__
+    {
+        initscr(); cbreak(); noecho();
+        input = getch();
+        return input;
+    }
+
+    #endif
+}
