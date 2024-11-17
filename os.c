f51b4609076fd15b2b35579729029a94b2953b68
SuperBypassUdin <24alaudin@mhs.usk.ac.id>
2024-11-17 13:46:00 +0700

    Update os.c

--- os.c
+++ os.c
@@ -1,30 +1,15 @@
-#include <stdio.h>
 #include <ncurses.h>
 
-// #ifdef __linux__
-// #endif
-
-int main(){
-    char input;
-    #ifdef __linux__
-        printf("Linux\n");
-        initscr(); 
-        cbreak(); 
-        noecho();
-        input = getch();
-        refresh();
-        getch();
-        endwin();
-        printf("%c", input);
-    #endif
-
-    #ifdef __unix__ 
-        printf("UNIX\n");
-    #endif
-
-    #ifdef __APPLE__
-        printf("Mac\n");
-    #endif
-
+int main() {
+    initscr();
+    cbreak();              // Non-buffered input
+    noecho();              // Non-echo input
+    printw("Tekan tombol untuk melanjutkan (tanpa Enter): ");
+    refresh();
+    char c = getch();      // Tunggu input
+    printw("\nAnda menekan: %c\n", c);
+    refresh();
+    getch();
+    endwin();
     return 0;
 }
