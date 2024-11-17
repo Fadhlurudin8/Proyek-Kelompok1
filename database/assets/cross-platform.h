89d43902aa49df45a9c4fe6d8f27a49c789c5dbd
Udin51 <148663014+Fadhlurudin8@users.noreply.github.com>
2024-11-17 18:29:55 +0700

    Update cross-platform.h

--- database/assets/cross-platform.h
+++ database/assets/cross-platform.h
@@ -20,10 +20,8 @@ char char_tanpa_buffer()
     #elif __linux__ || __unix__ || __APPLE__
         fflush(stdout);
         initscr(); cbreak(); noecho();
-        flushinp();
-        napms(100);  // Tunggu 100 milidetik
-        input = getch();
         endwin();
+        input = getch();
         return input;
     
     #endif
