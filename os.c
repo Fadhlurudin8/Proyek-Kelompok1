2ea06e9002ff43251421dbe5a11f42ee72685cb7
SuperBypassUdin <24alaudin@mhs.usk.ac.id>
2024-11-17 14:57:10 +0700

    Delete os.c

--- os.c
+++ os.c
@@ -1,15 +1,1 @@
-#include <ncurses.h>
 
-int main() {
-    initscr();
-    cbreak();              // Non-buffered input
-    noecho();              // Non-echo input
-    printw("Tekan tombol untuk melanjutkan (tanpa Enter): ");
-    refresh();
-    char c = getch();      // Tunggu input
-    printw("\nAnda menekan: %c\n", c);
-    refresh();
-    getch();
-    endwin();
-    return 0;
-}
