2f9f75ba54eea251ba39ae336e86cf868a2430f3
SuperBypassUdin <24alaudin@mhs.usk.ac.id>
2024-11-22 07:06:34 +0700

    Add files via upload

---
+++ database/assets/UI.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void kepalaProgram()
+{
+    printf("\n=== PROYEK PRAKTIKUM PEMROGRAMAN KELOMPOK 1 ===\n\n\n\n");
+}
+
+void menuUtama()
+{
+    printf("        WHO WANTS TO BE A        \n");
+    printf("           MILLIONAIRE           \n");
+    printf("                                 \n");
+    printf("=================================\n");
+    printf("*          1. Mulai             *\n");
+    printf("*          2. Peraturan         *\n");
+    printf("*          3. Keluar            *\n");
+    printf("=================================\n");
+    printf(">> ");
+}
+
+void peraturan()
+{
+    printf("================================================\n");
+    printf("|                                              |\n");
+    printf("|             |Peraturan Permainan|            |\n");
+    printf("| ============================================ |\n");
+    printf("| * 1. Pemain harus menjawab 10 pertanyaan   * |\n");
+    printf("| *    pilihan ganda secara berurutan dengan * |\n");
+    printf("| *    benar dengan 3 tingkat kesulitan yang * |\n");
+    printf("| *    dapat dipilih                         * |\n");
+    printf("| * 2. Jika pemain salah menjawab soal,      * |\n");
+    printf("| *    permainan berakhir.                   * |\n");
+    printf("| * 3. Skor akhir akan ditampilkan setelah   * |\n");
+    printf("| *    permainan berakhir                    * |\n");
+    printf(" ==============================================\n");
+    printf("                                     Kembali(y)\n");
+    printf(">> ");
+}
---
+++ database/assets/cross-platform.h
@@ -0,0 +1,36 @@
+// Platform Windows
+#ifdef _WIN32
+    #include <conio.h>
+// Platform UNIX, Linux, dan Mac
+#elif __linux__ || __unix__ || __APPLE__
+    #include <termios.h>
+    #include <unistd.h>
+#else
+    #error "Sistem operasi tidak didukung"
+#endif
+
+/* Fungsi untuk input char tanpa buffer 
+   Berguna untuk buat opsi pilihan kepada user*/
+char charTanpaBuffer()
+{
+    char input;
+    #ifdef _WIN32
+        input = _getch(); // input char tanpa buffer dan echo
+    #elif __linux__ || __unix__ || __APPLE__
+        struct termios oldt, newt;
+        tcgetattr(STDIN_FILENO, &oldt);           // Simpan pengaturan terminal saat ini
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);         // Nonaktifkan mode kanonik dan echo
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Terapkan pengaturan baru
+
+        input = getchar();                        // Ambil karakter dari input
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Kembalikan pengaturan terminal lama
+    #endif
+    
+    return input;
+}
+
+// Fungsi untuk membersihkan terminal
+void clearTerminal() {
+    printf("\e[1;1H\e[2J");
+}
