dda251604d8e4652a975f7a18f4c012c21d1d21f
SuperBypassUdin <24alaudin@mhs.usk.ac.id>
2024-11-22 07:05:58 +0700

    Create soal.h

---
+++ database/assets/soal.h
@@ -0,0 +1,46 @@
+// Struktur untuk soal
+typedef struct {
+    char *soal;
+    char jawaban;
+} Soal;
+
+// Kumpulan soal berdasarkan tingkat kesulitan
+Soal mudah[] = {
+    {"Ibukota Indonesia? \n a. Banten\n b. Jakarta\n c. Medan\n d. Surabaya", 'b'},
+    {"100 x 3 / 2 = ? \n a. 130\n b. 140\n c. 150\n d. 160", 'c'},
+    {"Planet terbesar di tata surya? \n a. Merkurius\n b. Pluto\n c. Saturnus\n d. Jupiter", 'd'},
+    {"Hewan tercepat di dunia?\n a. Harimau\n b. Macan\n c. Singa\n d. Cheetah", 'd'},
+    {"Lambang kimia untuk air?\n a. NaCl\n b. NaOH\n c. H2O\n d. O2", 'c'},
+    {"5 x 2 + 10 / 2 = ?\n a. 16\n b. 15\n c. 17\n d. 12", 'b'},
+    {"Gunung tertinggi di dunia?\n a. Everest\n b. Bromo\n c. Kangchenjunga\n d. Lhotse", 'a'},
+    {"Danau terbesar di dunia?\n a. Kaspia\n b. Superior\n c. Huron\n d. Sentani", 'a'},
+    {"Siapa nama presiden pertama Republik Indonesia?\n a. Megawati Soekarnoputri\n b. Mohammad Hatta\n c. B.J. Habibie\n d. Ir. Soekarno", 'd'},
+    {"Apa nama ibu kota negara Australia?\n a. Sydney\n b. Melbourne\n c. Canberra\n d. Perth", 'c'}
+};
+
+Soal sedang[] = {
+    {"Siapa penemu lampu pijar?\n a. Alexander Graham Bell\n b. Thomas Edison\n c. Nikola Tesla\n d. Albert Einstein", 'b'},
+    {"Apa nama proses di mana tumbuhan mengubah cahaya matahari menjadi energi?\n a. Fotosintesis\n b. Respirasi\n c. Transpirasi\n d. Fermentasi", 'a'},
+    {"Benua terbesar di dunia adalah...\n a. Afrika\n b. Asia\n c. Amerika Utara\n d. Eropa", 'b'},
+    {"Siapa penyanyi yang dikenal dengan lagu ""My Heart Will Go On""?\n a. Whitney Houston\n b. Celine Dion\n c. Mariah Carey\n d. Adele", 'b'},
+    {"Planet terdekat dengan matahari adalah...\n a. Venus\n b. Mars\n c. Merkurius\n d. Jupiter", 'c'},
+    {"Lagu kebangsaan Indonesia adalah...\n a. Tanah Airku\n b. Indonesia Raya\n c. Garuda Pancasila\n d. Satu Nusa Satu Bangsa", 'b'},
+    {"Sungai terpanjang di dunia adalah...\n a. Sungai Amazon\n b. Sungai Nil\n c. Sungai Yangtze\n d. Sungai Mississippi", 'b'},
+    {"Kepulauan yang dikenal sebagai ""Seribu Pulau"" adalah...\n a. Maluku\n b. Bali\n c. Kepulauan Seribu\n d. Nusa Tenggara", 'c'},
+    {"Bunga nasional Indonesia adalah...\n a. Melati\n b. Anggrek\n c. Mawar\n d. Kamboja", 'a'},
+    {"Apa nama unsur kimia dengan simbol Au?\n a. Argentum\n b. Aluminium\n c. Emas\n d. Perak", 'c'}
+};
+
+Soal susah[] = {
+    {"Siapa penulis novel ""Laskar Pelangi""?\n a. Andrea Hirata\n b. Pramoedya Ananta Toer\n c. Sapardi Djoko Damono\n d. Chairil Anwar", 'a'},
+    {"Apa nama sistem penulisan yang digunakan oleh suku Maya?\n a. Hieroglif\n b. Cuneiform\n c. Alphabets\n d. Logogram", 'a'},
+    {"Siapa yang menciptakan teori relativitas?\n a. Isaac Newton\n b. Albert Einstein\n c. Niels Bohr\n d. Stephen Hawking",'b'},
+    {"Siapa yang dikenal sebagai ""Bapak Pembangunan"" di Indonesia?\n a. Sukarno\n b. Suharto\n c. B.J. Habibie\n d. Megawati Soekarnoputri", 'b'},
+    {"Apa nama candi terbesar di Indonesia?\n a. Candi Borobudur\n b. Candi Prambanan\n c. Candi Mendut\n d. Candi Sewu", 'a'},
+    {"Apa nama teori yang menjelaskan asal usul kehidupan di bumi?\n a. Teori Evolusi\n b. Teori Big Bang\n c. Teori Abiogenesis\n d. Teori Relativitas", 'c'},
+    {"Siapa yang menulis buku ""Siti Nurbaya""?\n a. Hamka\n b. Marah Rusli\n c. Pramoedya Ananta Toer\n d. Chairil Anwar", 'b'},
+    {"Apa nama teori yang menyatakan bahwa semua spesies berasal dari spesies lain melalui proses evolusi?\n a. Teori Kreasionisme\n b. Teori Evolusi\n c. Teori Spesiasi\n d. Teori Adaptasi", 'b'},
+    {"Apa nama perjanjian yang mengakhiri Perang Dunia I?\n a. Perjanjian Versailles\n b. Perjanjian Trianon\n c. Perjanjian Saint-Germain\n d. Perjanjian Paris", 'a'},
+    {"Siapa ilmuwan yang mengembangkan hukum gravitasi?\n a. Galileo Galilei\n b. Isaac Newton\n c. Albert Einstein\n d. Johannes Kepler", 'b'},
+    {"Apa nama laut yang memisahkan Indonesia dan Australia?\n a. Laut China Selatan\n b. Laut Tasman\n c. Laut Arafuru\n d. Laut Sulu", 'b'}
+};
